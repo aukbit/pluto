6efdb9d7123134aa697635521c68c16995d5f281
paulo@workland <paulo@voncrank.com>
2017-06-13 17:23:23 +0100

    verify jws token validation

--- auth/jws/jws_test.go
+++ auth/jws/jws_test.go
@@ -4,6 +4,7 @@ import (
 	"crypto/rand"
 	"crypto/rsa"
 	"testing"
+	"time"
 )
 
 func TestSignAndVerify(t *testing.T) {
@@ -14,8 +15,8 @@ func TestSignAndVerify(t *testing.T) {
 	payload := &ClaimSet{
 		Iss: "http://google.com/",
 		Aud: "",
-		Exp: 3610,
-		Iat: 10,
+		Exp: time.Now().Unix() + 2,
+		Iat: time.Now().Unix(),
 	}
 
 	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
@@ -27,10 +28,19 @@ func TestSignAndVerify(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
 	err = Verify(token, &privateKey.PublicKey)
 	if err != nil {
 		t.Fatal(err)
+	}
+	c, err := Decode(token)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if c.Iss != "http://google.com/" {
+		t.Error("invalid decode")
+	}
+	if time.Now().Unix() > c.Exp {
+		t.Error("token as expired")
 	}
 }
 
