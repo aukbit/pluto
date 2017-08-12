316cca57eabbec27cd307875a582fc3b9a46dc10
paulo@homeland <paulo.martins@aukbit.com>
2017-08-12 18:37:56 +0100

    refactor log http requests

--- server/http_middleware.go
+++ server/http_middleware.go
@@ -2,15 +2,18 @@ package server
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"net/http"
-	"net/http/httputil"
 
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 
 	"github.com/aukbit/pluto/common"
 	"github.com/aukbit/pluto/server/router"
 )
+
+var methods = map[string]bool{"PUT": true, "POST": true, "PATCH": true}
 
 // loggerMiddleware Middleware that adds logger instance
 // available in handlers context and logs request
@@ -26,9 +29,17 @@ func loggerMiddleware(srv *Server) router.Middleware {
 			case "/_health":
 				break
 			default:
-				request, _ := httputil.DumpRequest(r, true)
+				var b zapcore.ObjectMarshaler
+				if _, ok := methods[r.Method]; ok {
+					json.NewDecoder(r.Body).Decode(b)
+				}
 				l.Info(fmt.Sprintf("%v %v %v %v", srv.Name(), r.Method, r.URL, r.Proto),
-					zap.Any("request", request),
+					zap.String("method", r.Method),
+					zap.String("url", r.URL.String()),
+					zap.String("proto", r.Proto),
+					zap.String("remote_addr", r.RemoteAddr),
+					zap.Any("header", r.Header),
+					zap.Object("body", b),
 				)
 			}
 			// also nice to have a logger available in context
