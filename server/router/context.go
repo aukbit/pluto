d852ac59d3d2132058efa2ce1905d620513944d3
paulo@workland <paulo@voncrank.com>
2017-08-15 18:06:12 +0100

    allow context to be changed in router

--- server/router/context.go
+++ server/router/context.go
@@ -10,7 +10,18 @@ type contextKey struct {
 	param string
 }
 
-// FromContext returns server instance from a context
+// FromContext returns value for repective param available in context
+// NOte: deprecated, please use FromContextParam
 func FromContext(ctx context.Context, param string) string {
 	return ctx.Value(contextKey{param}).(string)
+}
+
+// FromContextParam returns value for repective param available in context
+func FromContextParam(ctx context.Context, param string) string {
+	return ctx.Value(contextKey{param}).(string)
+}
+
+// WithContextParam returns a copy of ctx with param value associated.
+func WithContextParam(ctx context.Context, param, val string) context.Context {
+	return context.WithValue(ctx, contextKey{param}, val)
 }
